919d7f84594acd2a180bd511ce53e6b63b6b25bb
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-10-09 08:38:25 +0000

    Testing Views

--- todo/views.py
+++ todo/views.py
@@ -46,7 +46,6 @@ def toggle_item(request, item_id):
     return redirect('get_todo_list')
 
 
-
 def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.delete()
